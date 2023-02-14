a0d62b1d565a25fceda33aa004c5fe5121759ebe
Michael Fisher <fishermichaele@gmail.com>
2023-02-13 20:45:00 -0500

    feat: compile to dist/

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import puppeteer from 'puppeteer';
+import { URL } from 'url';
+import { mkdirSync, existsSync } from 'fs';
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false });
+const openBrowser = async () => {
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto('https://google.com');
   browser.close();
-})();
+};
+
+openBrowser();
